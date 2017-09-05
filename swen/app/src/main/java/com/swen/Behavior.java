23b056d56d8db3769cf3cee0ff7ad3b05f281144
DESKTOP-4487N69\Teon <fmj15@mails.tsinghua.edu.cn>
2017-09-05 18:08:21 +0800

    使用sharedPreference，故不再需要上一个commit中的方法

--- swen/app/src/main/java/com/swen/Behavior.java
+++ swen/app/src/main/java/com/swen/Behavior.java
@@ -2,6 +2,7 @@ package com.swen;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -19,53 +20,12 @@ public class Behavior
     private HashMap<String, Integer> keywordPreference = new HashMap<>();
     private HashMap<Integer, Integer> categoryPreference = new HashMap<>();
     private Context context;
+    private SharedPreferences sharedPreferences;
 
     public Behavior(Context context) {
         this.context = context;
+        sharedPreferences = context.getSharedPreferences("readingPreferences", Context.MODE_PRIVATE);
         readLocalPreference();
-    }
-
-    private void readLocalPreference() {
-        FileInputStream fis;
-        try {
-            fis = context.openFileInput("preference.dat");
-        } catch (FileNotFoundException e) {
-            try {
-                FileOutputStream fos = context.openFileOutput("preference.dat", Context.MODE_APPEND);
-                BufferedWriter bufferedWriter =
-                    new BufferedWriter(new OutputStreamWriter(fos));
-                for(int i = 1; i <= 12; i++) {
-                    bufferedWriter.write(0 + " ");
-                }
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                for(int i = 1; i < 13; i++) {
-                    categoryPreference.put(i, 0);
-                }
-                return;
-            } catch (IOException e1) {
-                e1.printStackTrace();
-                return;
-            }
-        }
-        BufferedReader bufferedReader =
-            new BufferedReader(new InputStreamReader(fis));
-        try {
-            String line = bufferedReader.readLine();
-            String[] split = line.split(" ");
-            for(int i = 1; i < 13; i++) {
-                int preference = Integer.valueOf(split[i - 1]);
-                categoryPreference.put(i, preference);
-            }
-            while((line = bufferedReader.readLine()) != null) {
-                split = line.split(" ");
-                keywordPreference.put(split[0],
-                    Integer.valueOf(split[1]));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void markHaveRead(News news) {}
