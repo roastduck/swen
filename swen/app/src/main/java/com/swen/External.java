caa7b4678352fe6fe8b0036121b6ee8b0221fabc
ZXCroon <zxcroon@gmail.com>
2017-09-06 20:13:37 +0800

    'Share' done; before test

--- swen/app/src/main/java/com/swen/External.java
+++ swen/app/src/main/java/com/swen/External.java
@@ -4,17 +4,33 @@ import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 public class External
 {
     private MediaPlayer player;
     private Thread thread;
     private final String urlFormat = "http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=5&text=%s";
+    private final Context context;
 
-    public class Target {}
+    public External(final Context context) {
+        this.context = context;
+        ShareSDK.initSDK(context);
+    }
 
-    public void share(News news, Target target) {}
+    public void share(News news) {
+        OnekeyShare oks = new OnekeyShare();
+        oks.disableSSOWhenAuthorize();
+        oks.setTitle(news.news_Title);
+        oks.setUrl(news.news_URL);
+        oks.setTitleUrl(news.news_URL);
+        oks.setImageUrl(news.news_Pictures);
+        oks.setSite("Swen");
+        oks.show(context);
+    }
 
-    public void readyRead(final Context context, final String str) {
+    public void readyRead(final String str) {
         thread = new Thread(new Runnable() {
             @Override
             public void run() {
