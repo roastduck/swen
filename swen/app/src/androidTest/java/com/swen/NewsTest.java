b675080a542d97e39e95af9c341de155a24a88af
roastduck <beantang.tang@gmail.com>
2017-09-08 10:08:34 +0800

    update NewsTest

--- swen/app/src/androidTest/java/com/swen/NewsTest.java
+++ swen/app/src/androidTest/java/com/swen/NewsTest.java
@@ -1,7 +1,5 @@
 package com.swen;
 
-import android.util.Log;
-
 import com.alibaba.fastjson.JSON;
 import org.junit.Before;
 import org.junit.Test;
@@ -62,9 +60,29 @@ public class NewsTest
     @Test
     public void testGetNewsPictures() throws Exception
     {
-        mNews.setNews_Pictures("http://news.xinhuanet.com/info/2015-10/26/134748860_14458157112331n.jpg http://news.xinhuanet.com/info/2015-10/26/134748860_14458157538451n.jpg http://news.xinhuanet.com/info/2015-10/26/134748860_14458157538551n.jpg http://news.xinhuanet.com/info/2015-10/26/134748860_14458157539251n.jpg http://news.xinhuanet.com/info/2015-10/26/134748860_14458157539351n.jpg");
-        List<String> picList = mNews.getNewsPictures();
+        News news = JSON.parseObject("{\"news_Pictures\":\"http://news.xinhuanet.com/info/2015-10/26/134748860_14458157112331n.jpg http://news.xinhuanet.com/info/2015-10/26/134748860_14458157538451n.jpg http://news.xinhuanet.com/info/2015-10/26/134748860_14458157538551n.jpg http://news.xinhuanet.com/info/2015-10/26/134748860_14458157539251n.jpg http://news.xinhuanet.com/info/2015-10/26/134748860_14458157539351n.jpg\"}", News.class);
+        List<String> picList = news.getNewsPictures();
         assertEquals(5, picList.size());
+        assertEquals("http://news.xinhuanet.com/info/2015-10/26/134748860_14458157112331n.jpg", picList.get(0));
+
+    }
+
+    @Test
+    public void testGetNewsPicturesSemicolon() throws Exception
+    {
+        News news = JSON.parseObject("{\"news_Pictures\":\"http://himg2.huanqiu.com/attachment2010/2016/0912/13/16/20160912011621140.png;http://himg2.huanqiu.com/attachment2010/2016/0912/13/16/20160912011630240.png;http://himg2.huanqiu.com/attachment2010/2016/0912/13/16/20160912011642992.png;http://himg2.huanqiu.com/attachment2010/2016/0912/13/16/20160912011655316.png;http://himg2.huanqiu.com/attachment2010/2016/0912/13/17/20160912011703267.png;http://himg2.huanqiu.com/statics/images/more-icoCopy.png\"}", News.class);
+        List<String> picList = news.getNewsPictures();
+        assertEquals(6, picList.size());
+        assertEquals("http://himg2.huanqiu.com/attachment2010/2016/0912/13/16/20160912011621140.png", picList.get(0));
+    }
+
+    @Test
+    public void testGetNewsPicturesSpace() throws Exception
+    {
+        // news_ID = 201609130413bf28e723640341ec9d5a98fea15500b2
+        News news = JSON.parseObject("{\"news_Pictures\":\" \"}", News.class);
+        List<String> picList = news.getNewsPictures();
+        assertTrue(picList.isEmpty());
     }
 
     @Test
