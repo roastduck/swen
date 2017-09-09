dd71b639258de1c93e6f3d2a846b2341313ca9c3
ZXCroon <zxcroon@gmail.com>
2017-09-09 21:46:09 +0800

    drawer, toolbar preliminarily completed without considering appearance

---
+++ swen/app/src/main/java/com/swen/Global.java
@@ -0,0 +1,19 @@
+package com.swen;
+
+public final class Global {
+    final static int[] categoryStrs = {
+            -1,
+            R.string.category_tech,
+            R.string.category_edu,
+            R.string.category_milit,
+            R.string.category_domes,
+            R.string.category_social,
+            R.string.category_culture,
+            R.string.category_car,
+            R.string.category_glob,
+            R.string.category_sports,
+            R.string.category_finance,
+            R.string.category_health,
+            R.string.category_enter
+    };
+}
--- swen/app/src/main/java/com/swen/MainActivity.java
+++ swen/app/src/main/java/com/swen/MainActivity.java
@@ -1,7 +1,17 @@
 package com.swen;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +21,26 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer);
+        final Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.menu_open, R.string.menu_close);
+        toggle.syncState();
+
+        MenuItem.ItemType type = MenuItem.ItemType.Text;
+        List<MenuItem> list = new ArrayList<>();
+        /*
+
+         */
+        ((ListView)findViewById(R.id.category_list_view)).setAdapter(new MenuItemAdapter(list, this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        return true;
     }
 }
