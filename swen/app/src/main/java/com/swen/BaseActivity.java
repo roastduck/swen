f522e037398c3a1b19060bccb8b09a1c45af32fd
ZXCroon <zxcroon@gmail.com>
2017-09-10 21:43:43 +0800

    MainActivity -> BaseActivity

---
+++ swen/app/src/main/java/com/swen/BaseActivity.java
@@ -0,0 +1,50 @@
+package com.swen;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer);
+        final Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.menu_open, R.string.menu_close);
+        toggle.syncState();
+
+        List<MenuItem> list = new ArrayList<>();
+        list.add(new MenuItem(R.string.category_mgmt, R.drawable.category_management, MenuItem.ItemType.TextWithIcon));
+        list.add(new MenuItem(R.string.no_image_mode, R.drawable.no_image, MenuItem.ItemType.TextWithIconSwitch));
+        list.add(new MenuItem(R.string.night_mode, R.drawable.night_mode, MenuItem.ItemType.TextWithIconSwitch));
+        list.add(new MenuItem(0, 0, MenuItem.ItemType.Nothing));
+
+        // add category
+        //
+        // for (int k = 1; k <= 12; ++k) {
+        //     list.add(new MenuItem(Global.categoryStrs[k], Global.categoryImgs[k], MenuItem.ItemType.TextWithIcon));
+        // }
+
+        ((ListView)findViewById(R.id.menu_list_view)).setAdapter(new MenuItemAdapter(list, this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        return true;
+    }
+}
---
+++ swen/app/src/main/java/com/swen/SearchResultsActivity.java
@@ -0,0 +1,29 @@
+package com.swen;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
+            //use the query to search your data somehow
+        }
+    }
+}
