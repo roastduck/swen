efaafdcd24be674e5336a34539551b12c65dda47
Teon Sean <416152680@qq.com>
2017-09-12 03:42:45 +0800

    试图对作为百科词条出现的词语进行过滤，但没有找到较好的办法，有待讨论。

--- swen/app/src/main/java/com/swen/ContentPolisher.java
+++ swen/app/src/main/java/com/swen/ContentPolisher.java
@@ -1,22 +1,38 @@
 package com.swen;
 
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class ContentPolisher
 {
     public static void addHref(News news) {
+        int i = 0;
+        ArrayList<String> keywords = new ArrayList<>();
+        for(News.WeightedKeyword w: news.Keywords) {
+            keywords.add(w.word);
+        }
         if(news.locations != null) {
             for (News.WordCnt wordCnt : news.locations) {
                 String word = wordCnt.word;
                 String replacement = "<a href=\"https://baike.baidu.com/item/"
                                      + word + "\">" + word + "</a>";
-                news.news_Content = news.news_Content.replaceAll(word, replacement);
+                news.news_Content = news.news_Content.replaceFirst(word, replacement);
             }
         }
         if(news.persons != null) {
             for (News.WordCnt wordCnt : news.persons) {
                 String word = wordCnt.word;
+                if(!keywords.contains(word)) {
+                    continue;
+                }
                 String replacement = "<a href=\"https://baike.baidu.com/item/"
                                      + word + "\">" + word + "</a>";
-                news.news_Content = news.news_Content.replaceAll(word, replacement);
+                news.news_Content = news.news_Content.replaceFirst(word, replacement);
             }
         }
         if(news.organizations != null) {
@@ -24,8 +40,9 @@ public class ContentPolisher
                 String word = wordCnt.word;
                 String replacement = "<a href=\"https://baike.baidu.com/item/"
                                      + word + "\">" + word + "</a>";
-                news.news_Content = news.news_Content.replaceAll(word, replacement);
+                news.news_Content = news.news_Content.replaceFirst(word, replacement);
             }
         }
     }
+
 }
