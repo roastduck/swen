3bd3fc57e5ec976c28b5e851269a66edce40f598
ZXCroon <zxcroon@gmail.com>
2017-09-13 04:36:06 +0800

    fix: get stuck at first search

--- swen/app/src/main/java/com/swen/RecommendationActivity.java
+++ swen/app/src/main/java/com/swen/RecommendationActivity.java
@@ -5,6 +5,11 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 
+import com.swen.promise.Callback;
+import com.swen.promise.Promise;
+
+import org.ansj.splitWord.analysis.ToAnalysis;
+
 import cn.sharesdk.framework.ShareSDK;
 
 /**
@@ -16,6 +21,18 @@ public class RecommendationActivity extends NewsListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Do nothing but launching the library
+        // It takes long to do ToAnalysis.parse for the first time
+        new Promise<Object, Object>(new Callback<Object, Object>() {
+            @Override
+            public Object run(Object o) {
+                ToAnalysis.parse("This is to launch the library.");
+                return new Object();
+            }
+        }, null);
+
+
         mAppendableList = new AppendableNewsList(10, null, null, true, ((ApplicationWithStorage)getApplication()).getBehavior());
         initialize();
 
